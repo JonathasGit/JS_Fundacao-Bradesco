7b1d4bdd8f64f4a0c810f834f7c1479c96e8c42d
JonathasGit <jonathas-25@hotmail.com>
2022-03-04 09:34:56 -0300

    #2 aula

--- capitulo 2/JS Aninhamento ou Encadeamento  de if/salario1.js
+++ capitulo 2/JS Aninhamento ou Encadeamento  de if/salario1.js
@@ -27,7 +27,49 @@ if(horas_mes > 180){ // Se as horas do mês for maior que 180, então faça
 }
 
 if(horas_mes < 135){ //se as horas do mês for menor que 135, então
-    document.write(' Valor mínimo invalido');
+    document.write(' Valor mínimo invalido !!! ');
 }
 
 //Validação das horas trabalhadas de 25 a 50
+document.write('Valor da hora trabalhada (25 a 50) = ' + valor_hora + '<br>');
+if(valor_hora > 50){ // Se o valor da hora for maior que 50   
+    document.write('Valor máximo excedido')
+}
+if(valor_hora < 25){ // Se valor for menor que 25
+    document.write('Valor minino invalido' );    
+}
+
+document.write('Percentual de Desconto do INSS = ' + desconto_INSS);
+
+if(desconto_INSS > 15){ // Se valor do inss foir maior que 15 
+document.write('Valor máximo excedido');
+}
+if(desconto_INSS <5){
+    document.write('Valor mínimo inválido');
+}
+
+document.write('================');
+document.write('Salário Bruto' = salario_bruto);
+document.write('Total de descontos' + total_desconto);
+document.write('Salário Liquido' + salario_liquido);
+
+
+// Saida de dados com aninhamneto de ifs
+
+if(salario_liquido > 10500){ // se salario foi maior que 10.500
+    document.write('Salário Elevado, maior que 10.500' );
+
+} else
+if(salario_liquido > 6000){
+    document.write('Salário Satisfatorio!');
+} else
+if(salario_liquido > 3000){
+    document.write('Salário Moderado');
+
+} else {
+    document.write('Salário insatisfatório');
+
+}
+
+
+
