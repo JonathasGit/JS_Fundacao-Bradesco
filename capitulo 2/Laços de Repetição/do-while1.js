19e9d51dd2ecfc2fb89b1116258a168f6debbbb9
JonathasGit <jonathas-25@hotmail.com>
2022-04-12 09:18:50 -0300

    #2 slide 50

--- capitulo 2/Laços de Repetição/do-while1.js
+++ capitulo 2/Laços de Repetição/do-while1.js
@@ -15,7 +15,7 @@
 
 
 
-var horas_mes; // - Qunatidade de horas trabalhadas
+var horas_mes; // - Quantidade de horas trabalhadas
 var valor_hora; // Valor da hora trabalhada
 var desconto_INSS; // Desconto inss
 var total_descontos;
@@ -36,5 +36,61 @@ total_descontos = (desconto_INSS/100) * salario_bruto;
 salario_liquido = salario_bruto - total_descontos;
 
 //*Saida dos dados*/ */
+document.write('<p> <b>Demonstrativo para cálculo do salário líquido</b> ');
 
-// parado em 4:10
+document.write('<p>Quantidade de horas trabalhadas (135 a 180) = <b>' + horas_mes + '</b>' );
+if (horas_mes > 180){
+    document.write('... (<b>Valor máximo excedido</b> )');
+}
+if(horas_mes <135){
+    document.write('... (<b>Valor minimo invalido</b>)');
+}
+
+//Cálcula o valor da hora trabalhada 
+document.write('<p>Valor da  hora trabalhada (25 a 50) = <b>' + valor_hora + '</b>' );
+if (valor_hora > 50){
+    document.write('... (<b>Valor máximo excedido</b> )');
+}
+if(valor_hora < 25){
+    document.write('... (<b>Valor minimo invalido</b>)');
+}
+
+//Calculo do desconto 
+document.write('<p>Desconto INSS (5 a 10) = <b>' + desconto_INSS + '</b>' );
+if (desconto_INSS > 10){
+    document.write('... (<b>Valor máximo excedido</b> )');
+}
+if(desconto_INSS < 5){
+    document.write('... (<b>Valor minimo invalido</b>)');
+}
+
+
+//Mostra resultado
+document.write('<p> Salario Bruto = ' + salario_bruto);
+document.write ('<p>Total de Desconto INSS = ' + desconto_INSS );
+document.write('<p>Salario Liquido' + salario_liquido); 
+
+
+//Saida de dados com Aninhamento de If
+
+if(salario_liquido > 10500){
+    document.write('...(<b>Salário Elevado!</b> Maior que R$10.500,00)');
+} else {
+    if(salario_liquido > 6000) {
+        document.write('... (<b>Salário Bom</b> Maior que o R$6.000,00! )');
+    } else{
+        if(salario_liquido > 3000){
+            document.write('...(<b>Saalrio até que bom!</b> Maior que R$3.000,00 )');
+        } else {
+            document.writeI('...(<b>Salário Não atende o que quer conquistar!!!</b> Menor que R$3.000,00)');
+        }
+    }
+}
+
+document.write('<p>==================================================');
+
+resposta = window.confirm('Deseja continuar ?');
+
+while (resposta == true);
+
+// Slide 50 do capitulo 2 
